019bf9d5f684aa5ea4adaf0522f9706f22d99f67
Akash Gupta <gr8akash1994@gmail.com>
2024-03-27 17:37:41 +0530

    Project for kafka

---
+++ kafka/src/main/java/com/akash/kafka/kafka/KafkaProducer.java
@@ -0,0 +1,9 @@
+package com.akash.kafka.kafka;
+
+import org.springframework.stereotype.Service;
+
+//Here with this class we will produce the messsage and then put it to the topics.
+@Service
+public class KafkaProducer {
+
+}
